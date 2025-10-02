a2db6dce7fbda6569db2fa1d630215eb3bcc7511
Hafsa Rana <hafsamrana25@gmail.com>
2025-10-02 20:21:03 +0100

    create search bar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+export default function SearchBar({ onSearch }: SearchBarProps) {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search artworks..."
+      />
+      <button onClick={() => onSearch(query)}>Search</button>
+    </div>
+  );
+}
