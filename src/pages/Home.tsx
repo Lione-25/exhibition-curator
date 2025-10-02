3a93ad95ebc51b09624dace3fff6f775e25b53ae
Hafsa Rana <hafsamrana25@gmail.com>
2025-10-02 20:30:55 +0100

    create home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+import SearchBar from "../components/SearchBar";
+import ArtGrid from "../components/ArtGrid";
+import ViewerModal from "../components/ViewerModal";
+import { fetchMetArt } from "../api/met";
+
 export default function Home() {
-  return <div></div>;
+  const [artworks, setArtworks] = useState<any[]>([]);
+  const [selected, setSelected] = useState<any | null>(null);
+
+  async function handleSearch(query: string) {
+    const results = await fetchMetArt(query);
+    setArtworks(results);
+  }
+
+  return (
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      <ArtGrid artworks={artworks} onSelect={setSelected} />
+      <ViewerModal art={selected} onClose={() => setSelected(null)} />
+    </div>
+  );
 }
