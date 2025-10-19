1122c7b018342b19ca2271cc5c72a87797faa58d
Hafsa Rana <hafsamrana25@gmail.com>
2025-10-19 22:11:24 +0100

    update title

--- src/pages/Exhibition.tsx
+++ src/pages/Exhibition.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useExhibition } from "../context/ExhibitionContext";
 import ArtGrid from "../components/ArtGrid";
 import ViewerModal from "../components/ViewerModal";
@@ -7,6 +7,10 @@ export default function Exhibition() {
   const [selected, setSelected] = useState<any | null>(null);
   const { exhibition } = useExhibition();
   if (exhibition.length === 0) return <p>No artworks added yet.</p>;
+
+  useEffect(() => {
+    document.title = "My Exhibition | The Virtual Gallery";
+  }, []);
 
   return (
     <div>
