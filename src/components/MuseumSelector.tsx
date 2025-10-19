651b1f1e15ffb480f801c840bfc2d9fbd701d768
Hafsa Rana <hafsamrana25@gmail.com>
2025-10-19 08:29:57 +0100

    fix styling issues in museum selector

--- src/components/MuseumSelector.tsx
+++ src/components/MuseumSelector.tsx
@@ -1,24 +1,24 @@
 import { useState } from "react";
-import "../styles/MuseumSelector.module.css";
+import styles from "../styles/MuseumSelector.module.css";
 
 function MuseumSelector() {
   const [museum, setMuseum] = useState<"met" | "science">("met");
   const [dropdownOpen, setDropdownOpen] = useState(false);
   return (
-    <div className="museum-selector">
+    <div className={styles.museumSelector}>
       <button
-        style={{
-          //   background: "#f5f5f5",
-          border: "1px solid #ccc",
-          borderRadius: "6px",
-          //   padding: "0.4rem 0.75rem",
-          cursor: "pointer",
-          fontSize: "0.9rem",
-          //   color: "#333",
-          transition: "background 0.2s",
-          //   marginBottom: "1rem",
-        }}
-        // className="museum-button"
+        // style={{
+        //   //   background: "#f5f5f5",
+        //   border: "2px solid #333",
+        //   borderRadius: "6px",
+        //   //   padding: "0.4rem 0.75rem",
+        //   cursor: "pointer",
+        //   fontSize: "0.9rem",
+        //   //   color: "#333",
+        //   transition: "background 0.2s",
+        //   //   marginBottom: "1rem",
+        // }}
+        className={styles.museumButton}
         onClick={() => setDropdownOpen((prev) => !prev)}
       >
         {museum === "met"
@@ -27,7 +27,7 @@ function MuseumSelector() {
         ▼
       </button>
       {dropdownOpen && (
-        <ul className="museum-dropdown">
+        <ul className={styles.museumDropdown}>
           <li
             onClick={() => {
               setMuseum("met");
