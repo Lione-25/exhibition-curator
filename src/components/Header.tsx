fad41b60ca7e6951ef71a591194ff74a491ed3b1
Hafsa Rana <hafsamrana25@gmail.com>
2025-10-19 21:42:22 +0100

    update project name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <div className="header-left">
-        <h1 className="app-title">Art Explorer</h1>
+        <h1 className="app-title">The Virtual Gallery</h1>
         <nav className="nav-links">
           <Link to="/">Home</Link>
           <Link to="/exhibition">Exhibition</Link>
