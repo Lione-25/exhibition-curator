36a57b002464e46f4adfd486c1bc7301b71d3d4b
Hafsa Rana <hafsamrana25@gmail.com>
2025-10-19 10:26:26 +0100

    clean up unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Exhibition from "./pages/Exhibition";
 import Header from "./components/Header";
